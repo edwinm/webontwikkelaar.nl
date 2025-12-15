d665926a6146c9c232ee948e46d6dbd22e27598f
Edwin Martin <edwin@bitstorm.org>
2025-12-15 21:28:38 +0100

    mv dest to dist

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
     return {
         dir: {
             input: ".",        // Input directory
-            output: "dest",     // Output directory
+            output: "dist",     // Output directory
         },
 
         // Template formats
