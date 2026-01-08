3bdca66944d9fe144d8ceaf4726d4119fe18e745
Edwin Martin <edwin@bitstorm.org>
2026-01-08 21:25:59 +0100

    Include RSS feeds

--- _data/feeds.js
+++ _data/feeds.js
@@ -1,6 +1,23 @@
+import Parser from 'rss-parser';
+
+const parser = new Parser();
+
+const blogs = ['https://www.w3.org/news/feed/', 'http://feeds.feedburner.com/EsnextNews']
+
 export default async function() {
     return {
-        posts: [],
+        posts: await getBlogs(),
         lastUpdated: new Date()
     }
+}
+
+async function getBlogs() {
+
+    const feedPromises = blogs.map(blog => parser.parseURL(blog))
+
+    const feeds = await Promise.all(feedPromises);
+
+    console.log(JSON.stringify(feeds));
+
+    return feeds;
 }
