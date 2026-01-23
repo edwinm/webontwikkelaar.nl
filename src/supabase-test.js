e79570e31f6f6bf6517701118c6ea901ae7fad99
Edwin Martin <edwin@bitstorm.org>
2026-01-23 23:10:06 +0100

    update query

--- src/supabase-test.js
+++ src/supabase-test.js
@@ -4,12 +4,23 @@ import { createClient } from '@supabase/supabase-js'
 
 // Docs: https://supabase.com/docs/reference/javascript/introduction
 
+// CREATE VIEW conferences_with_location AS
+// SELECT
+// conf.*,
+//     c.latitude,
+//     c.longitude
+// FROM conferences conf
+// LEFT JOIN cities c ON c.name = conf.city;
+
+const today = new Intl.DateTimeFormat('sv-SE').format(new Date());
 
 const supabase = createClient(process.env.SupabaseUrl, process.env.SupabaseAnonKey);
 
 const { data, error } = await supabase
-    .from('conferences')
-    .select();
+    .from('conferences_with_location')
+    .select()
+    .gte('start_date', today)
+    .order('start_date', { ascending: true });
 
 console.log('error', error);
 
