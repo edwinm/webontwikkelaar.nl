b78a0476b473416f15ef25dd650fc1bf6257a014
Edwin Martin <edwin@bitstorm.org>
2026-02-11 23:37:05 +0100

    use horse remedy against hanging 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,6 @@
-import { getGlobalDispatcher } from 'undici';
-
 export default function(eleventyConfig) {
     eleventyConfig.on('eleventy.after', async () => {
-      await getGlobalDispatcher().close();
-      // process.exit(0); // horse remedy
+        process.exit(0); // horse remedy against hanging 11ty
     });
 
     eleventyConfig.addPassthroughCopy("assets");
