f0a5559f026fae6fb3a85f1cfd3b68e18ff60249
Edwin Martin <edwin@bitstorm.org>
2026-02-22 23:04:31 +0100

    bg/fg colors, move date to top

--- .eleventy.js
+++ .eleventy.js
@@ -30,9 +30,6 @@ export default function(eleventyConfig) {
             day: 'numeric',
             month: 'long',
             year: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit',
-            hour12: false,
             timeZone: 'Europe/Amsterdam'
         });
     });
